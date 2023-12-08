1b8bab42bf0890deaf65665821fc49046db305fd
GrantMitchell24 <GrantMitchell2423@yahoo.com>
2023-12-08 11:32:13 -0600

    deploying on heroku

--- server/server.js
+++ server/server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "..", 'client/build')));
+  app.use(express.static(path.join(__dirname, "..", 'client/dist')));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve( __dirname, "..", 'client/build/index.html'))
+    res.sendFile(path.resolve( __dirname, "..", 'client/dist/index.html'))
   })
 }
 
