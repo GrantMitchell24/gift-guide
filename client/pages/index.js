aaea0acf44b82a724c30fe62c30b876214b5ce58
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-12-10 12:37:45 -0600

    added logout functionality to navbar logout button

--- client/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AuthPage } from "./AuthPage";
-export { default as HomePage } from "./HomePage";
-export { default as Logout } from "./Logout";
-export { default as PrivatePage } from "./PrivatePage";
-export { default as PurchasedPage } from "./PurchasedPage";
--- server/models/index.js
+++
@@ -1,5 +0,0 @@
-const Sample = require('./Sample');
-const Group = require('./Group');
-const User = require('./User');
-
-module.exports = { Sample, User, Group };
