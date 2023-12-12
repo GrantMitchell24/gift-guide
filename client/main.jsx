eb29251b29b51098c6d97be3a80362c8b2f62c61
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-12-11 20:50:24 -0600

    adding light/dark mode from Chakra

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "./components/theme";
+import * as ReactDOM from "react-dom/client";
 
 //import Chakra
 import { ChakraProvider } from "@chakra-ui/react";
@@ -9,6 +12,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
